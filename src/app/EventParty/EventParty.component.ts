b1953cdb17e8e5ba5c4e788e1510c248cd9485ca
NCanhhieu <109597988+NCanhhieu@users.noreply.github.com>
2022-11-21 19:40:33 +0700

    update layout and menu test 4

---
+++ src/app/EventParty/EventParty.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component(
+  {
+    selector: 'app-EVPT',
+    templateUrl:'./EventParty.component.html'
+  })
+export class EventPartyComponent extends Component{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {EventPartyComponent} from "./EventParty/EventParty.component";
+import {HomeComponent} from "./Home/Home.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HomeComponent, EventPartyComponent
   ],
   imports: [
     BrowserModule
