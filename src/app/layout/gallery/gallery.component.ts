2dc67a05f69da947ed7ae5ad9ce8b3b056ba4ac3
NCanhhieu <109597988+NCanhhieu@users.noreply.github.com>
2022-12-10 08:47:04 +0700

    update gallery

--- src/app/layout/gallery/gallery.component.ts
+++ src/app/layout/gallery/gallery.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Data} from "../../interface/eventtype.interface";
+
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit {
+
+  gallery:  Data[] = [];
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(){// ham nay tu dong chay khi print giao dien
+
+    const url='http://localhost:5000/gallery';
+    this.http.get<Data[]>(url).subscribe(data => {
+      this.gallery = data;
+    });
+
+
+  }
+
 
 }
